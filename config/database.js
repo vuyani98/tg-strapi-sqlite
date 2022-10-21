8d60760b599163f76d96f6f540088c569f3e32dc
Vuyani Ncube <nvuyani307@gmail.com>
2022-10-21 04:34:44 +0200

    database fix 1

--- config/database.js
+++ config/database.js
@@ -1,12 +1,10 @@
 const path = require('path');
-const bookshelf = require('bookshelf');
 
 module.exports = ({ env }) => ({
   connection: {
     client: 'sqlite',
-    connector: bookshelf,
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      filename: env('DATABASE_FILENAME', path.join(__dirname, '..', '.tmp/data.db')),
     },
     useNullAsDefault: true,
   },

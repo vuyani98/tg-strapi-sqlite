68b43eda8d91c99495731f1263a32218950b961a
Vuyani Ncube <nvuyani307@gmail.com>
2022-10-25 08:09:58 +0200

    database file

--- config/database.js
+++ config/database.js
@@ -6,7 +6,8 @@ module.exports = ({ env }) => ({
     client: 'sqlite',
     connector: bookshelf,
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      //filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      filename: env('DATABASE_FILENAME', path.join(__dirname, '..', '.tmp/data.db')),
     },
     useNullAsDefault: true,
   },
